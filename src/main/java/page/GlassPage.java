c3f901619a810245c1570f0d566ee3bdf1723f34
Pivi14 <pivarcsiadam@gmail.com>
2020-07-23 10:07:54 +0200

    permissonTest

--- src/main/java/page/GlassPage.java
+++ src/main/java/page/GlassPage.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.HashMap;
 
 public class GlassPage extends WebPage{
     @FindBy(id = "glass-general-panel")
     WebElement glassPage;
-
+    @FindBy(xpath = "//a[@data-target='permissions']")
+    WebElement permissionsButton;
 
     public GlassPage(WebDriver driver) {
         super(driver);
@@ -54,5 +58,14 @@ public class GlassPage extends WebPage{
 
     public Integer checkComponentsWithId(String componentId){
         return driver.findElements(By.xpath("//tr[@data-component-id='" + componentId + "']/td[@class='components-table__name']/div/a")).size();
+    }
+
+    public void clickPermissionsButton(){
+        permissionsButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("glass-permissions-panel")));
+    }
+
+    public boolean getPermissionsInGlass(String key){
+        return driver.findElement(By.xpath("//b[text()='" + key + "']/ancestor::tr/td[3]/div")).getAttribute("class").equals("glass-true-icon");
     }
 }
---
+++ src/main/java/test/GlassTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.*;
+
+public class GlassTest implements DriverSetup{
+    HomePage homePage;
+    MainPage mainPage;
+    GlassPage glassPage;
+    PermissionPage permissionPage;
+
+    @BeforeAll
+    void pageSetup(){
+        mainPage = new MainPage(driver);
+        homePage = new HomePage(driver);
+        glassPage = new GlassPage(driver);
+        permissionPage = new PermissionPage(driver);
+        mainPage.goToMainPage();
+        mainPage.login(System.getenv("USER"), System.getenv("PASSWORD"));
+        homePage.waitForLoad();
+    }
+
+    @BeforeEach
+    void goToHomePage(){
+        homePage.goToPage();
+    }
+
+    @Test
+    void testVerifyPermissionMatrix(){
+        permissionPage.goToPage();
+        permissionPage.savePermissionData();
+        glassPage.goToPage();
+        glassPage.clickPermissionsButton();
+        Assertions.assertEquals(permissionPage.getPermissionFromProjectPage().get("Browse Projects"), glassPage.getPermissionsInGlass("Browse Projects"));
+        Assertions.assertEquals(permissionPage.getPermissionFromProjectPage().get("Create Issues"), glassPage.getPermissionsInGlass("Create Issues"));
+        Assertions.assertEquals(permissionPage.getPermissionFromProjectPage().get("Edit Issues"), glassPage.getPermissionsInGlass("Edit Issues"));
+    }
+}
